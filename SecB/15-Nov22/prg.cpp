de148b7b74c33856a83b73903508b68cf338ec2b
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2012-11-22 15:04:32 -0500

    df

---
+++ SecB/15-Nov22/prg.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class PhoneNo{
+  char _name[41];
+  int _number;
+public:
+  PhoneNo(const char* name="", int number = 0){
+    strncpy(_name, name, 40);
+    _name[40] = 0;
+    _number = number;
+  }
+  ostream& print(ostream& os)const{
+    return os<<_name<<": "<<_number;
+  }
+  operator const char*(){
+    return _name;
+  }
+  operator int(){
+    return _number;
+  }
+};
+
+ostream& operator<<(ostream&os, const PhoneNo& P){
+  return P.print(os);
+}
+
+int main(){
+  PhoneNo F("Fardad Soleimanloo", 3151538);
+  cout<<F<<endl;
+  cout<<(const char*)F<<endl;
+  cout<<(int)F<<endl;
+  return 0;
+}
